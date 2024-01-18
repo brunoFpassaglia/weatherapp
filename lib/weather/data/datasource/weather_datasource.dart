92647440d9b9eb3c5c9ceec13d8595c6194b2e44
Bruno Passaglia <bruno.passaglia2013@gmail.com>
2024-01-18 16:49:49 -0300

    add weather datasource implementation

---
+++ lib/weather/data/datasource/weather_datasource.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart';
+
+abstract interface class WeatherDatasource {
+  Future<String> getWeather(double lat, double long);
+}
+
+class WeatherDatasourceImpl implements WeatherDatasource {
+  final Client httpClient;
+
+  WeatherDatasourceImpl({required this.httpClient});
+
+  @override
+  Future<String> getWeather(double lat, double long) async {
+    var uri = Uri.https('api.openweathermap.org', 'data/2.5/weather', {
+      'lat': lat.toString(),
+      'lon': long.toString(),
+      'appid': 'fcc7658a4dfb9229ef701d7ad9e52c34'
+    });
+    var response = await httpClient.get(uri);
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Error getting the weather info');
+    }
+  }
+}
---
+++ test/unit/weather_datasource_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/weather/data/datasource/weather_datasource.dart';
+
+void main() {
+  test('test get weather request', () async {
+    var client = http.Client();
+
+    var datasource = WeatherDatasourceImpl(httpClient: client);
+    var response = await datasource.getWeather(-25.3959, -51.4626);
+    expect(response, isA<String>());
+    print(response);
+  });
+}
